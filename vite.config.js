db0d2da29cae78c6f32a2ab443181b6565dd2a99
robacodigo87 <erios8@ucol.mx>
2024-10-18 21:40:33 -0600

    Preparación para publicar

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://123-edwin.github.io/accesibility',
 })
